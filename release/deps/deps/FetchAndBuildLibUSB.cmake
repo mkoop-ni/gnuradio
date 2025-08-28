dd2073843e3960addedf9d7f798dfe6a5ea28784
Clemens Felber <clemens.felber@emerson.com>
2025-08-28 12:58:28 +0200

    uhd: libusb: switch to version 1.0.29 supporting VS2022

--- release/deps/deps/FetchAndBuildLibUSB.cmake
+++ release/deps/deps/FetchAndBuildLibUSB.cmake
@@ -1,11 +1,11 @@
 # Module responsible for fetching, building, and installing LibUSB for
 # consumption by subproject
 set(LIBUSB_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/libusb")
-set(LIBUSB_URL "https://github.com/libusb/libusb/archive/refs/tags/v1.0.26.tar.gz")
-set(LIBUSB_HASH "a09bff99c74e03e582aa30759cada218ea8fa03580517e52d463c59c0b25e240")
+set(LIBUSB_URL "https://github.com/libusb/libusb/archive/refs/tags/v1.0.29.tar.gz")
+set(LIBUSB_HASH "7C2DD39C0B2589236E48C93247C986AE272E27570942B4163CB00A060FCF1B74")
 if(WIN32)
     set(LIBUSB_CONF_ARGS "")
-    set(LIBUSB_BUILD_ARGS cd msvc && msbuild /property:Configuration=Release libusb_2019.sln)
+    set(LIBUSB_BUILD_ARGS cd msvc && msbuild /property:Configuration=Release libusb.sln)
     set(LIBUSB_INSTALL_COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BINARY_DIR=${CMAKE_BINARY_DIR} -P ${CMAKE_CURRENT_LIST_DIR}/installLibUSB.cmake)
 else()
     set(LIBUSB_CONF_ARGS /configure --prefix=${LIBUSB_INSTALL_DIR} --disable-udev --disable-dependency-tracking)
