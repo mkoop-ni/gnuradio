d3d8cb4c0c8d83da528211a661defcb75f9c4da1
Clemens Felber <clemens.felber@emerson.com>
2025-09-08 08:25:53 +0200

    uhd: switch to UHD 4.9.0.0 release

--- release/deps/deps/FetchAndBuildUHD.cmake
+++ release/deps/deps/FetchAndBuildUHD.cmake
@@ -1,6 +1,6 @@
 set(UHD_VERSION 4.9.0.0) # version is also used in custom install command
-set(UHD_URL https://github.com/EttusResearch/uhd/archive/refs/tags/v${UHD_VERSION}-rc1.zip)
-set(UHD_HASH d37a095826b76f98660572ab7d651eb8068517d7c80fb4e24fc1470ec241a8b9)
+set(UHD_URL https://github.com/EttusResearch/uhd/archive/refs/tags/v${UHD_VERSION}.zip)
+set(UHD_HASH 23DEAD8C3A2C6A57C169853E96D09360494824EFECAD2365CFBB1D2A5D2FA486)
 set(UHD_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/uhd)
 # Define some optional CMAKE_ARGS
 # Solution has the drawback of generating cmake warnings when optional
