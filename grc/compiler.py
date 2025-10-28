mkoop-ni/gnuradio

commit 83c643a64f892937fc60f5c6f257cf89f70ba1dd

MacOS Side updates

CMakeLists.txt
@@ -57,7 +57,6 @@ include(GrVersion) # setup version info
 
 # Set minimum version requirements
 include(GrMinReq)
-
 ########################################################################
 # Setup Boost for global use (within this build)
 # Do this before enabling testing support, as it depends
@@ -170,6 +169,33 @@ if(DEFINED CACHE{SYSCONFDIR} AND NOT GR_IGNORE_SYSCONFDIR_SET)
 endif()
 # cmake-format: on
 
+# Set location of config/prefs files in /etc
+# Special exception if prefix is /usr so we don't make a /usr/etc.
+set(GR_CONF_DIR
+    etc
+    CACHE PATH "Path to install config files")
+string(COMPARE EQUAL "${CMAKE_INSTALL_PREFIX}" "/usr" isusr)
+if(GR_BUILD_INSTALLER)
+        set(SYSCONFDIR
+        ${GR_CONF_DIR}
+        CACHE PATH "Relative system configuration directory" FORCE
+    )
+elseif(isusr)
+    set(SYSCONFDIR
+        "/${GR_CONF_DIR}"
+        CACHE PATH "System configuration directory")
+else(isusr)
+    set(SYSCONFDIR
+        "${CMAKE_INSTALL_PREFIX}/${GR_CONF_DIR}"
+        CACHE PATH "System configuration directory" FORCE)
+endif(isusr)
+
+set(GR_PKG_CONF_DIR
+    ${SYSCONFDIR}/${CMAKE_PROJECT_NAME}/conf.d
+    CACHE PATH "Path to install package configs")
+set(GR_PREFSDIR
+    ${SYSCONFDIR}/${CMAKE_PROJECT_NAME}/conf.d
+    CACHE PATH "Path to install preference files")
 
 option(ENABLE_PERFORMANCE_COUNTERS "Enable block performance counters" ON)
 if(ENABLE_PERFORMANCE_COUNTERS)
@@ -247,27 +273,38 @@ if(WIN32)
 else(WIN32)
     file(RELATIVE_PATH prefix_relative_to_lib "${prefix}/${GR_LIBRARY_DIR}" "${prefix}")
 endif(WIN32)
-file(RELATIVE_PATH GR_CONF_DIR_relative_to_prefix "${prefix}" "${GR_CONF_DIR_FULL}")
-file(RELATIVE_PATH GR_PREFSDIR_relative_to_prefix "${prefix}" "${GR_PREFSDIR_FULL}")
+if(NOT GR_BUILD_INSTALLER)
+    file(RELATIVE_PATH SYSCONFDIR_relative_to_prefix "${prefix}" "${SYSCONFDIR}")
+    file(RELATIVE_PATH GR_PREFSDIR_relative_to_prefix "${prefix}" "${GR_PREFSDIR}")
+else()
+    set(SYSCONFDIR_relative_to_prefix "${SYSCONFDIR}")
+    set(GR_PREFSDIR_relative_to_prefix "${GR_PREFSDIR}")
+endif()
 
 ########################################################################
 # On Apple only, set install name and use rpath correctly, if not already set
 ########################################################################
 if(APPLE)
-    if(NOT CMAKE_INSTALL_NAME_DIR)
+    cmake_policy(SET CMP0068 NEW)
+    set(MACOSX_BUNDLE_ICON_FILE "${CMAKE_SOURCE_DIR}/release/resources/icons/gnuradio_logo_icon-square.ico")
+    if(NOT CMAKE_INSTALL_NAME_DIR AND NOT GR_BUILD_INSTALLER)
         set(CMAKE_INSTALL_NAME_DIR
             ${CMAKE_INSTALL_PREFIX}/${GR_LIBRARY_DIR}
-            CACHE PATH "Library Install Name Destination Directory" FORCE)
+            CACHE PATH "Library Install Name Destination Directory" )
     endif(NOT CMAKE_INSTALL_NAME_DIR)
     if(NOT CMAKE_INSTALL_RPATH)
         set(CMAKE_INSTALL_RPATH
-            ${CMAKE_INSTALL_PREFIX}/${GR_LIBRARY_DIR}
+            "@loader_path;@loader_path/../lib;@loader_path/../bin;@loader_path/..;@loader_path/../../../..;@loader_path/../../../../..;${CMAKE_INSTALL_PREFIX}/${GR_LIBRARY_DIR};${CMAKE_INSTALL_PREFIX}/bin"
             CACHE PATH "Library Install RPath" FORCE)
     endif(NOT CMAKE_INSTALL_RPATH)
     if(NOT CMAKE_BUILD_WITH_INSTALL_RPATH)
+        set(GR_USE_INSTALL_RPATH ON)
+        if(GR_BUILD_INSTALLER)
+            set(GR_USE_INSTALL_RPATH OFF)
+            set(CMAKE_INSTALL_RPATH_USE_LINK_PATH ON)
+        endif()
         set(CMAKE_BUILD_WITH_INSTALL_RPATH
-            ON
-            CACHE BOOL "Do Build Using Library Install RPath" FORCE)
+            ${GR_USE_INSTALL_RPATH} CACHE BOOL "Do Build Using Library Install RPath" FORCE)
     endif(NOT CMAKE_BUILD_WITH_INSTALL_RPATH)
 endif(APPLE)
 

grc/CMakeLists.txt
@@ -29,25 +29,25 @@ GR_PYTHON_CHECK_MODULE(
 
 GR_PYTHON_CHECK_MODULE_RAW(
     "pygobject >= 2.28.6"
-    "import os; os.add_dll_directory('${GTK_BIN_DIR}');"
+    "import gi; assert gi.version_info >= (2, 28, 6)"
     PYGI_FOUND
 )
 
 GR_PYTHON_CHECK_MODULE_RAW(
     "Gtk (GI) >= 3.10.8"
-    "import os; os.add_dll_directory('${GTK_BIN_DIR}');"
+    "import gi; gi.require_version('Gtk', '3.0');"
     GTK_GI_FOUND
 )
 
 GR_PYTHON_CHECK_MODULE_RAW(
     "Cairo (GI) >= 1.0"
-    "import os; os.add_dll_directory('${GTK_BIN_DIR}');"  # Cairo 1.13.0
+    "import gi; gi.require_foreign('cairo', 'Context')"  # Cairo 1.13.0
     CAIRO_GI_FOUND
 )
 
 GR_PYTHON_CHECK_MODULE_RAW(
     "PangoCairo (GI) >= 1.0"
-    "import os; os.add_dll_directory('${GTK_BIN_DIR}');"  # pangocairo 1.36.3
+    "import gi; gi.require_version('PangoCairo', '1.0')"  # pangocairo 1.36.3
     PANGOCAIRO_GI_FOUND
 )
 
@@ -79,7 +79,7 @@ if(NOT CMAKE_CROSSCOMPILING)
         GTK_GI_FOUND
         CAIRO_GI_FOUND
         PANGOCAIRO_GI_FOUND
-        NUMPY_FOUND
+        # NUMPY_FOUND
     )
 endif(NOT CMAKE_CROSSCOMPILING)
 

gnuradio-runtime/CMakeLists.txt
@@ -93,17 +93,18 @@ gr_register_component(
 
 # find thrift quietly to determine whether it should be enabled by default
 gr_find_package(THRIFT ${GR_THRIFT_MIN_VERSION} QUIET)
-set(THRIFT_BIN ${Thrift_INCLUDE_DIR}/../bin/thrift)
-set(THRIFT_HAS_THREADFACTORY_H TRUE)
-set_target_properties(thrift::thrift
-    PROPERTIES
-        INTERFACE_COMPILE_DEFINITIONS THRIFT_HAS_THREADFACTORY_H
-)
+
 option(ENABLE_CTRLPORT_THRIFT "Enable ControlPort Thrift support" ${THRIFT_FOUND})
 
 if(ENABLE_CTRLPORT_THRIFT)
     # ensure we have thrift, loudly, since it is enabled
     gr_find_package(THRIFT ${GR_THRIFT_MIN_VERSION} REQUIRED)
+    set(THRIFT_BIN ${Thrift_INCLUDE_DIR}/../bin/thrift)
+    set(THRIFT_HAS_THREADFACTORY_H TRUE)
+    set_target_properties(Thrift::thrift
+        PROPERTIES
+        INTERFACE_COMPILE_DEFINITIONS THRIFT_HAS_THREADFACTORY_H
+)
 endif()
 
 ########################################################################

gnuradio-runtime/lib/CMakeLists.txt
@@ -138,7 +138,6 @@ target_link_libraries(
     PRIVATE
         Boost::program_options
         Boost::system
-        Boost::filesystem
         libunwind::libunwind)
 
 # Address linker issues with std::filesystem on Centos 8 and Debian
@@ -219,7 +218,7 @@ if(ENABLE_GR_CTRLPORT)
                 ${CMAKE_CURRENT_SOURCE_DIR}/controlport/thrift/thrift_application_base.cc
                 # add files built by compiling gnuradio.thrift
                 ${gnuradio_thrift_generated_sources})
-        target_link_libraries(gnuradio-runtime PRIVATE thrift::thrift)
+        target_link_libraries(gnuradio-runtime PRIVATE Thrift::thrift)
 
         target_compile_definitions(gnuradio-runtime PUBLIC GR_CTRLPORT)
 

gr-blocks/lib/CMakeLists.txt
@@ -176,7 +176,7 @@ add_library(gnuradio-blocks ${BLOCKS_SOURCES})
 target_link_libraries(gnuradio-blocks PUBLIC gnuradio-runtime Volk::volk)
 
 if(SNDFILE_FOUND)
-    target_link_libraries(gnuradio-blocks PRIVATE SndFile::sndfile)
+    target_link_libraries(gnuradio-blocks PRIVATE sndfile::sndfile)
 endif()
 
 target_include_directories(

gr-fft/CMakeLists.txt
@@ -10,7 +10,7 @@
 ########################################################################
 include(GrBoost)
 
-gr_find_package(FFTW3)
+gr_find_package(FFTW3f)
 
 ########################################################################
 # Register component
@@ -25,7 +25,7 @@ gr_register_component(
         gr-blocks
     REQUIRED_TPLS
         Boost_FOUND
-        FFTW3_FOUND
+        FFTW3f_FOUND
 )
 
 ########################################################################

gr-fft/lib/CMakeLists.txt
@@ -13,7 +13,7 @@ add_library(gnuradio-fft fft.cc fft_v_fftw.cc goertzel_fc_impl.cc goertzel.cc wi
 target_link_libraries(
     gnuradio-fft
     PUBLIC gnuradio-runtime Volk::volk
-    PRIVATE FFTW3::fftw3f)
+    PRIVATE fftw3f::fftw3f)
 
 # Address linker issues with std::filesystem on Centos 8 and Debian
 target_link_libraries(

gr-zeromq/CMakeLists.txt
@@ -10,10 +10,10 @@
 # Setup dependencies
 ########################################################################
 gr_find_package(ZeroMQ)
-add_library(ZeroMQ::ZeroMQ INTERFACE IMPORTED)
-set_target_properties(
-    ZeroMQ::ZeroMQ PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${ZeroMQ_INCLUDE_DIRS}
-                                INTERFACE_LINK_LIBRARIES ${ZeroMQ_LIBRARIES})
+# add_library(ZeroMQ::ZeroMQ INTERFACE IMPORTED)
+# set_target_properties(
+#     ZeroMQ::ZeroMQ PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${ZeroMQ_INCLUDE_DIRS}
+#                                 INTERFACE_LINK_LIBRARIES ${ZeroMQ_LIBRARIES})
 ########################################################################
 # Register component
 ########################################################################

commit 338d7719477ff611831b0c2c3383f7377e909561

installer: remove global blocks path and use prefix
- grc: remove global_blocks_path preference and use prefix path
  This change is taken from conda-forge grc patch:
  https://github.com/conda-forge/gnuradio-feedstock/blob/main/recipe/0002-grc-Remove-global_blocks_path-preference-and-use-pre.patch
- gr-utils: removed default newmod path from conf.in and CMakeLists.
- Improvements and fixes on commit 4bd4c65f8b68c4d6babce4f91a9931e69e4c4abf (MacOS Side updates)
  - generate constants.py in source dir
  - add install_prefix arg in required methods
- fix for prefix and config dir relative paths
  - Problem introduced in commit 83c643a.
  - Fixed by using GR_CONF_DIR_FULL and GR_PREFSDIR_FULL
  to compute relative paths for SYSCONFDIR_relative_to_prefix
  and GR_PREFSDIR_relative_to_prefix respectively.

Signed-off-by: Abhishek Verma <abhishek.verma@ni.com>

CMakeLists.txt
@@ -274,8 +274,8 @@ else(WIN32)
     file(RELATIVE_PATH prefix_relative_to_lib "${prefix}/${GR_LIBRARY_DIR}" "${prefix}")
 endif(WIN32)
 if(NOT GR_BUILD_INSTALLER)
-    file(RELATIVE_PATH SYSCONFDIR_relative_to_prefix "${prefix}" "${SYSCONFDIR}")
-    file(RELATIVE_PATH GR_PREFSDIR_relative_to_prefix "${prefix}" "${GR_PREFSDIR}")
+    file(RELATIVE_PATH SYSCONFDIR_relative_to_prefix "${prefix}" "${GR_CONF_DIR_FULL}")
+    file(RELATIVE_PATH GR_PREFSDIR_relative_to_prefix "${prefix}" "${GR_PREFSDIR_FULL}")
 else()
     set(SYSCONFDIR_relative_to_prefix "${SYSCONFDIR}")
     set(GR_PREFSDIR_relative_to_prefix "${GR_PREFSDIR}")

gr-utils/modtool/CMakeLists.txt
@@ -16,8 +16,6 @@ set(GR_PKG_MODTOOL_DATA_DIR ${GR_PKG_DATA_DIR}/modtool/templates)
 ########################################################################
 # Create and install the modtool conf file
 ########################################################################
-file(TO_NATIVE_PATH ${CMAKE_INSTALL_PREFIX}/${GR_PKG_MODTOOL_DATA_DIR}/gr-newmod newmoddir)
-
 configure_file(
     ${CMAKE_CURRENT_SOURCE_DIR}/modtool.conf.in
     ${CMAKE_CURRENT_BINARY_DIR}/modtool.conf

gr-utils/modtool/modtool.conf.in
@@ -3,4 +3,4 @@
 # ~/.gnuradio/config.conf
 
 [modtool]
-newmod_path = @newmoddir@
+newmod_path =

grc/CMakeLists.txt
@@ -109,14 +109,6 @@ if(ENABLE_GRC)
 ########################################################################
 # Create and install the grc and grc-docs conf file
 ########################################################################
-file(TO_NATIVE_PATH ${CMAKE_INSTALL_PREFIX}/${GRC_BLOCKS_DIR} blocksdir)
-file(TO_NATIVE_PATH ${CMAKE_INSTALL_PREFIX}/${GRC_EXAMPLES_DIR} examplesdir)
-if(CMAKE_INSTALL_PREFIX STREQUAL "/usr")
-    # linux binary installs: append blocks and examples dir with prefix /usr/local
-    set(blocksdir ${blocksdir}:/usr/local/${GRC_BLOCKS_DIR})
-    set(examplesdir ${examplesdir}:/usr/local/${GRC_EXAMPLES_DIR})
-endif(CMAKE_INSTALL_PREFIX STREQUAL "/usr")
-
 if(UNIX)
     find_program(GRC_XTERM_EXE
         NAMES x-terminal-emulator gnome-terminal konsole xfce4-terminal urxvt xterm foot
@@ -148,6 +140,30 @@ configure_file(
 install(
     FILES ${CMAKE_CURRENT_BINARY_DIR}/00-grc-docs.conf
     DESTINATION ${GR_PREFSDIR_relative_to_prefix}
+)
+
+########################################################################
+# Configure Python files that need variable substitution.
+# Copy the generated file to source directory for development/testing.
+########################################################################
+configure_file(
+    ${CMAKE_CURRENT_SOURCE_DIR}/core/Constants.py.in
+    ${CMAKE_CURRENT_BINARY_DIR}/core/Constants.py
+@ONLY)
+
+# Copy the generated file to source directory for development/testing
+add_custom_command(
+    OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/core/Constants.py
+    DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/core/Constants.py
+    COMMAND ${CMAKE_COMMAND} -E copy
+        ${CMAKE_CURRENT_BINARY_DIR}/core/Constants.py
+        ${CMAKE_CURRENT_SOURCE_DIR}/core/Constants.py
+    COMMENT "Copying generated Constants.py to source directory"
+)
+
+# Create a target to ensure the copy happens
+add_custom_target(copy_constants_to_source ALL
+    DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/core/Constants.py
 )
 
 ########################################################################

grc/compiler.py
@@ -39,7 +39,8 @@ def main(args=None):
         prefs=gr.prefs(),
         version=gr.version(),
         version_parts=(gr.major_version(),
-                       gr.api_version(), gr.minor_version())
+                       gr.api_version(), gr.minor_version()),
+        install_prefix=gr.prefix(),
     )
     platform.build_library()
 

grc/core/Config.py
@@ -6,7 +6,7 @@ SPDX-License-Identifier: GPL-2.0-or-later
 
 
 import os
-from os.path import expanduser, normpath, expandvars, exists
+from os.path import expanduser, normpath, expandvars, exists, join
 from collections import OrderedDict
 
 from ..main import get_state_directory, get_config_file_path
@@ -20,9 +20,10 @@ class Config(object):
 
     hier_block_lib_dir = os.environ.get('GRC_HIER_PATH', get_state_directory())
 
-    def __init__(self, version, version_parts=None, name=None, prefs=None):
+    def __init__(self, version, install_prefix, version_parts=None, name=None, prefs=None):
         self._gr_prefs = prefs if prefs else DummyPrefs()
         self.version = version
+        self.install_prefix = install_prefix
         self.version_parts = version_parts or version[1:].split(
             '-', 1)[0].split('.')[:3]
         self.enabled_components = self._gr_prefs.get_string(
@@ -32,12 +33,14 @@ class Config(object):
 
     @property
     def block_paths(self):
-        paths_sources = (
+        paths_sources = [
             self.hier_block_lib_dir,
             os.environ.get('GRC_BLOCKS_PATH', ''),
             self._gr_prefs.get_string('grc', 'local_blocks_path', ''),
-            self._gr_prefs.get_string('grc', 'global_blocks_path', ''),
-        )
+            normpath(join(self.install_prefix, Constants.GRC_BLOCKS_DIR)),
+        ]
+        if self.install_prefix == "/usr":
+            paths_sources.append(normpath(join("/usr/local", Constants.GRC_BLOCKS_DIR)))
 
         collected_paths = sum((paths.split(os.pathsep)
                                for paths in paths_sources), [])
@@ -52,7 +55,10 @@ class Config(object):
 
     @property
     def example_paths(self):
-        return [self._gr_prefs.get_string('grc', 'examples_path', '')]
+        return [
+            self._gr_prefs.get_string('grc', 'examples_path', ''),
+            normpath(join(self.install_prefix, Constants.GRC_EXAMPLES_DIR)),
+        ]
 
     @property
     def default_flow_graph(self):

grc/core/Constants.py.in
@@ -20,6 +20,8 @@ DATA_DIR = os.path.dirname(__file__)
 BLOCK_DTD = os.path.join(DATA_DIR, 'block.dtd')
 DEFAULT_FLOW_GRAPH = os.path.join(DATA_DIR, 'default_flow_graph.grc')
 DEFAULT_FLOW_GRAPH_ID = 'default'
+GRC_BLOCKS_DIR = os.path.normpath("@GRC_BLOCKS_DIR@")
+GRC_EXAMPLES_DIR = os.path.normpath("@GRC_EXAMPLES_DIR@")
 
 PROJECT_DEFAULT_DIR = 'gnuradio'
 GRC_SUBDIR = 'grc'

grc/grc.conf.in
@@ -3,9 +3,8 @@
 # ~/.config/gnuradio/grc.conf
 
 [grc]
-global_blocks_path = @blocksdir@
 local_blocks_path =
-examples_path = @examplesdir@
+examples_path =
 default_flow_graph =
 xterm_executable = @GRC_XTERM_EXE@
 canvas_font_size = 8

grc/gui/Config.py
@@ -30,9 +30,8 @@ class Config(CoreConfig):
 
     gui_prefs_file = os.environ.get('GRC_PREFS_PATH', get_config_file_path())
 
-    def __init__(self, install_prefix, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         CoreConfig.__init__(self, *args, **kwargs)
-        self.install_prefix = install_prefix
         Constants.update_font_size(self.font_size)
 
         self.parser = configparser.ConfigParser()

grc/gui_qt/Config.py
@@ -1,7 +1,8 @@
 import os
-from os.path import expanduser, normpath, expandvars, exists
+from os.path import expanduser, normpath, expandvars, exists, join
 from collections import OrderedDict
 
+import Constants
 from ..core.Config import Config as CoreConfig
 from ..main import get_config_file_path
 from qtpy import QtCore
@@ -13,9 +14,8 @@ class Config(CoreConfig):
 
     gui_prefs_file = os.environ.get('GRC_QT_PREFS_PATH', get_config_file_path('grc_qt.conf'))
 
-    def __init__(self, install_prefix, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         CoreConfig.__init__(self, *args, **kwargs)
-        self.install_prefix = install_prefix
         self.qsettings = QtCore.QSettings(self.gui_prefs_file, QtCore.QSettings.IniFormat)
 
     @property
@@ -28,7 +28,7 @@ class Config(CoreConfig):
             self.hier_block_lib_dir,
             os.environ.get('GRC_BLOCKS_PATH', ''),
             self._gr_prefs.get_string('grc', 'local_blocks_path', ''),
-            self._gr_prefs.get_string('grc', 'global_blocks_path', ''),
+            normpath(join(self.install_prefix, Constants.GRC_BLOCKS_DIR)),
             self.qsettings.value('grc/custom_block_paths', ''),
         )
 

grc/tests/test_block_flags.py
@@ -1,5 +1,6 @@
 
 from grc.core.blocks._flags import Flags
+from grc.core import Constants
 
 
 def test_simple_init():

grc/tests/test_cheetah_converter.py
@@ -2,6 +2,7 @@
 
 import functools
 import grc.converter.cheetah_converter as parser
+from grc.core import Constants
 
 
 def test_basic():

grc/tests/test_compiler.py
@@ -12,6 +12,7 @@ from os import path
 import tempfile
 
 from grc.compiler import main
+from grc.core import Constants
 
 
 def test_compiler(capsys):

grc/tests/test_cpp.py
@@ -12,6 +12,7 @@ from os import path
 import tempfile
 
 from grc.compiler import main
+from grc.core import Constants
 
 
 def test_cpp(capsys):

grc/tests/test_examples.py
@@ -4,12 +4,14 @@ from os import path
 
 import pytest
 
+from gnuradio import gr
 from grc.core.platform import Platform
 
 try:
     os.mkdir(path.join(path.dirname(__file__), 'resources/tests'))
 except FileExistsError:
     pass
+from grc.core import Constants
 
 # Gather blocks
 BLOCK_PATHS = []
@@ -88,6 +90,7 @@ def test_all_examples(example):
         name='GNU Radio Companion Compiler',
         prefs=None,
         version='0.0.0',
+        install_prefix=gr.prefix(),
     )
     platform.build_library(BLOCK_PATHS)
 

grc/tests/test_generator.py
@@ -9,7 +9,9 @@ import logging
 import platform as sys_platform
 from os import path
 
+from gnuradio import gr
 from grc.core.platform import Platform
+from grc.core import Constants
 
 
 def test_generator():
@@ -51,6 +53,7 @@ def test_generator():
         name='GNU Radio Companion Compiler',
         prefs=None,
         version='0.0.0',
+        install_prefix=gr.prefix(),
     )
     log.info("Building library...")
     platform.build_library(block_paths)

grc/tests/test_xml_parser.py
@@ -8,6 +8,7 @@ from os import path
 import sys
 
 from grc.converter import flow_graph
+from grc.core import Constants
 
 
 def test_flow_graph_converter():